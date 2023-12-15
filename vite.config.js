693f4dff68d3eec4b98f741efa0f44623a862edd
Tamilarasan <Tamilarasanpandiyan@gmail.com>
2023-12-15 11:56:00 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 
 dotenv.config();
 
 export default defineConfig({
-  plugins: [react()
-    ,reactRefresh()],
+  plugins: [react()],
   server: {
     host: true,
   },
-  define: {
-    'tsparticles': 'tsparticles/dist/tsparticles.esm',
- },
 });
